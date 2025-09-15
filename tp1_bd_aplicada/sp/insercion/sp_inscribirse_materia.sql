ff962aab44cf56c434d67e59ad29840c42b0658d
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-09-15 11:38:46 -0300

    Procemiento para anotarse a materias terminado, Transaccionado

--- tp1_bd_aplicada/sp/insercion/sp_inscribirse_materia.sql
+++ tp1_bd_aplicada/sp/insercion/sp_inscribirse_materia.sql
@@ -14,64 +14,70 @@ CREATE OR ALTER PROCEDURE [negocio].[sp_Inscribirse_Materia]
 @ANO               INT 
 AS 
 BEGIN 
-    IF NOT EXISTS 
-    (
-        SELECT 1
-        FROM [db_tp_bd_aplicada].[negocio].[Comision]
-        WHERE @TIPO_DOC     = TipoDocDocente    AND 
-              @DNI_DOC      = NroDocDocente     AND 
-              @NRO_COM      = NroComision       AND 
-              @COD_MAT      = CodMateria        AND 
-              @CUATRIMESTRE = Cuatrimestre      AND 
-              @D_SEM        = DiaSemana         AND 
-              @ANO          = Año
-    )
-    BEGIN 
-        PRINT('La comisión a la que se quiere anotar no existe dentro')
-        RETURN 0 -- La comisión no existe
-    END 
-        
 
-    IF EXISTS 
-    (
-        SELECT 1
-        FROM [db_tp_bd_aplicada].[negocio].[Se_Inscribe]
-        WHERE @TIPO_DOC     = TipoDocente    AND 
-              @DNI_DOC      = NroDocDocente     AND 
-              @NRO_COM      = NroComision       AND 
-              @COD_MAT      = CodMAteria        AND 
-              @CUATRIMESTRE = Cuatrimestre      AND 
-              @D_SEM        = DiaSemana         AND 
-              @ANO          = Año               AND 
-              @TIPO_ALU     = TipoAlumno        AND 
-              @DNI_ALU      = NroDocAlumno      
-    )
-    BEGIN 
-        PRINT('Usted ya fué inscripto a esta comisión, no puede volverse a anotar')
-        RETURN 2 -- Ya está anotado a esta comisión
-    END
-        
+    BEGIN TRANSACTION 
+    BEGIN TRY 
+        IF NOT EXISTS 
+        (
+            SELECT 1
+            FROM [db_tp_bd_aplicada].[negocio].[Comision]
+            WHERE @TIPO_DOC      =   TipoDocDocente    AND 
+                @DNI_DOC         =   NroDocDocente     AND 
+                @NRO_COM         =   NroComision       AND 
+                @COD_MAT         =   CodMateria        AND 
+                @CUATRIMESTRE    =   Cuatrimestre      AND 
+                @D_SEM           =   DiaSemana         AND 
+                @ANO             =   Año
+        )
+            RAISERROR('La comisión no existe en la base de datos', 21, 1)
+            
 
-    INSERT INTO [db_tp_bd_aplicada].[negocio].[Se_Inscribe]    
-        (TipoDocente, NroDocDocente, TipoAlumno, NroDocAlumno, NroComision, CodMAteria, Cuatrimestre, DiaSemana, Año)
-    VALUES 
-    (
-        @TIPO_DOC,
-        @DNI_DOC,
-        @TIPO_ALU,
-        @DNI_ALU,
-        @NRO_COM,
-        @COD_MAT,
-        @CUATRIMESTRE,
-        @D_SEM,
-        @ANO
-    )
-    RETURN 1
+        IF EXISTS 
+        (
+            SELECT 1
+            FROM [db_tp_bd_aplicada].[negocio].[Se_Inscribe]
+            WHERE @TIPO_DOC      =    TipoDocente       AND 
+                @DNI_DOC         =    NroDocDocente     AND 
+                @NRO_COM         =    NroComision       AND 
+                @COD_MAT         =    CodMAteria        AND 
+                @CUATRIMESTRE    =    Cuatrimestre      AND 
+                @D_SEM           =    DiaSemana         AND 
+                @ANO             =    Año               AND 
+                @TIPO_ALU        =    TipoAlumno        AND 
+                @DNI_ALU         =    NroDocAlumno      
+        )
+            RAISERROR('Usted ya está anotado a esta comisión', 21, 1)
+            
+
+        INSERT INTO [db_tp_bd_aplicada].[negocio].[Se_Inscribe]    
+            (TipoDocente, NroDocDocente, TipoAlumno, NroDocAlumno, NroComision, CodMAteria, Cuatrimestre, DiaSemana, Año)
+        VALUES 
+        (
+            @TIPO_DOC,
+            @DNI_DOC,
+            @TIPO_ALU,
+            @DNI_ALU,
+            @NRO_COM,
+            @COD_MAT,
+            @CUATRIMESTRE,
+            @D_SEM,
+            @ANO
+        )
+        COMMIT TRANSACTION
+    END TRY 
+    BEGIN CATCH
+        DECLARE @MJE_ERROR NVARCHAR(2000),
+                @SEVERIDAD INT, 
+                @ESTADO INT 
+
+        SELECT  @MJE_ERROR = ERROR_MESSAGE(),
+                @SEVERIDAD = ERROR_SEVERITY(),
+                @ESTADO    = ERROR_STATE() 
+
+        RAISERROR(@MJE_ERROR, @SEVERIDAD, @ESTADO)
+
+        ROLLBACK TRANSACTION
+    END CATCH
 END
 
-SELECT * FROM negocio.Se_Inscribe
-
-DECLARE @RES INT = 0
-EXEC @RES = [db_tp_bd_aplicada].[negocio].[sp_Inscribirse_Materia] 1,'13028164',1, '11618387', 4, '2930', 1, 4, 2025
-SELECT @RES
 
