1bd2fffb52c007850ec908ecf41ee0f2ab67ea86
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-09-15 02:17:30 -0300

    Vista Todas las asignación terminada

---
+++ tp1_bd_aplicada/vw/vw_comisiones_asignadas.sql
@@ -0,0 +1,19 @@
+
+USE db_tp_bd_aplicada
+
+GO
+CREATE OR ALTER VIEW vw_Comisiones_Asignadas
+AS 
+(
+    SELECT COM.NroComision Com, DOC.IDTipo Tipo, DOC.NroDoc Dni,DOC.Nombre Nom, DOC.Apellido Ape,
+        COUNT(COM.NroComision) OVER (PARTITION BY COM.NroDocDocente) AS Cant_Comisiones_Asignadas  
+    FROM 
+    [db_tp_bd_aplicada].[negocio].[Comision] COM 
+    INNER JOIN
+    [db_tp_bd_aplicada].[negocio].[Persona] DOC
+    ON  COM.TipoDocDocente = DOC.IDTipo AND 
+        COM.NroDocDocente = DOC.NroDoc
+)
+
+GO
+SELECT * FROM vw_Comisiones_Asignadas
