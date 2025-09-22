a04bd92ef3cf5d1bbd72cd6f0ea6e186c0c402ed
FedeManzano <154226686+FedeManzano@users.noreply.github.com>
2025-09-21 21:42:43 -0300

    SP Insertar test 4 terminado

--- db_alquileres_vehiculos/test/test_sp_insertar_cliente.sql
+++ db_alquileres_vehiculos/test/test_sp_insertar_cliente.sql
@@ -53,7 +53,7 @@ EXEC
 EXEC [db_utils].[library].[sp_Assert_Equals] 2,'Normal, Normalización de datos',@ESPERADO, @OBTENIDO, NULL
 --------------------------------------------------------------------------------------------------------------------
 /***
-    TEST 2
+    TEST 3
     INGRESO FALLIDO, CARACTER ESPECIAL EN EL APELLIDO
     EL REGISTRO NO SE DEBE INSETAR
 
@@ -76,10 +76,33 @@ EXEC
 
 EXEC [db_utils].[library].[sp_Assert_Equals] 3,'Fallido, Caracter especial en el apellido',@ESPERADO, @OBTENIDO, NULL
 
+---------------------------------------------------------------------------------------------------------------------
 
+/***
+    TEST 4
+    INGRESO FALLIDO, CARACTER ESPECIAL EN EL NOMBRE
+    EL REGISTRO NO SE DEBE INSETAR
+
+    ESP = -1 <=> OBT = -1
+*/
+
+SET @ESPERADO = -1
+SET @OBTENIDO = -1
+
+EXEC 
+[db_alquileres_vehiculos].[negocio].[sp_Insertar_Cliente] 1, 
+'23111222', 
+'marc+os', 
+'alfonso', 
+'rivadavia 5000, primera junta', 
+'alberto@gmail.com',  
+'1987-05-03',
+'5401146547552',
+@OBTENIDO OUTPUT
+
+EXEC [db_utils].[library].[sp_Assert_Equals] 4,'Fallido, Caracter especial en el NOMBRE',@ESPERADO, @OBTENIDO, NULL
 
 
 
 SELECT * FROM [db_alquileres_vehiculos].[negocio].[Cliente]
-
 DELETE FROM [db_alquileres_vehiculos].[negocio].[Cliente]
